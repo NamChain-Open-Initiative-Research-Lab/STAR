9359aaf38dc5d57d94105892842fc3812d2a8941
Ramaguru Radhakrishnan <7790256+ramagururadhakrishnan@users.noreply.github.com>
2025-03-15 12:01:14 +0530

    Swin Transformer for Multi-Label Classification

--- src/model.py
+++ src/model.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn as nn
-import timm  # Hugging Face Pretrained Model
+import timm
 
-class SwinTransformerModel(nn.Module):
-    def __init__(self, num_classes=2):
-        super(SwinTransformerModel, self).__init__()
-        
-        # Load Swin Transformer (pretrained)
+class SwinTransformerMultiLabel(nn.Module):
+    def __init__(self, num_classes):
+        super(SwinTransformerMultiLabel, self).__init__()
         self.model = timm.create_model("swin_base_patch4_window7_224", pretrained=True, num_classes=num_classes)
+        self.model.head = nn.Sequential(
+            nn.Linear(self.model.head.in_features, num_classes),  # Adjust final layer
+            nn.Sigmoid()  # Multi-label classification
+        )
 
     def forward(self, x):
         return self.model(x)
